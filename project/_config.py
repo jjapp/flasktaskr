86cf18f58aba26a334b6f2a7509758e8cb234c0f
JOHN APPERT <appertjt@gmail.com>
2016-04-07 11:12:24 -0700

    fixed css issues

---
+++ project/_config.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 06 08:02:04 2016
+
+@author: appertjt
+"""
+
+import os
+
+#grab the folder where this script lives
+basedir=os.path.abspath(os.path.dirname(__file__))
+
+DATABASE='flastaskr.db'
+USERNAME='admin'
+PASSWORD='admin'
+WTF_CSRF_ENABLED=True
+SECRET_KEY='my_precious'
+
+#define the full path for the database
+
+DATABASE_PATH=os.path.join(basedir, DATABASE)
---
+++ project/run.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 06 13:11:31 2016
+
+@author: appertjt
+"""
+
+#project/run.py
+
+from views import app
+app.run(debug=True)

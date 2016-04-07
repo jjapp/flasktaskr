1020afee16f090a029338968d938173c0d154248
JOHN APPERT <appertjt@gmail.com>
2016-04-07 16:07:11 -0700

    changed to sqlalchemy

--- project/_config.py
+++ project/_config.py
@@ -13,9 +13,12 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 DATABASE='flastaskr.db'
 USERNAME='admin'
 PASSWORD='admin'
-WTF_CSRF_ENABLED=True
+CSRF_ENABLED=True
 SECRET_KEY='my_precious'
 
 #define the full path for the database
 
 DATABASE_PATH=os.path.join(basedir, DATABASE)
+
+#the database uri
+SQLALCHEMY_DATABASE_URI='sqlite:///'+DATABASE_PATH
